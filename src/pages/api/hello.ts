5b0fb8a82d571c298006007559252544ab2d2ad9
sigridjineth <sigrid.jinhyung@gmail.com>
2023-03-27 16:10:46 +0900

    feat: add langchain dependency and make the basic implementation
    remove .next folder

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { OpenAI } from 'langchain'
+
+const model = new OpenAI({
+  // TODO: don't post this! make it environment variable!
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.0,
+})
 
 type Data = {
   name: string
 }
 
-export default function handler(
+const callModel = async (text: string) => {
+  const res = await model.call(text);
+  console.log(res);
+  return res;
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'GET') {
+    const tempText: string = "What would be a good company name a company that makes colorful socks?";
+    const modelResponse: string = await callModel(tempText);
+    res.status(200).json({ name: modelResponse })
+  } else {
+    res.status(405).json({ name: 'Method Not Allowed' })
+  }
 }

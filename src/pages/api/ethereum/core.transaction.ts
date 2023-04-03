54b53f2b33a081545fb6a5f437e57e0646eaf070
sigridjineth <sigrid.jinhyung@gmail.com>
2023-04-03 15:47:29 +0900

    Feature Milestone #1: Langchain Implementation

---
+++ src/pages/api/ethereum/core.transaction.ts
@@ -0,0 +1,47 @@
+// 1. gpt3.5 turbo
+// 2. flipside query
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { OpenAI } from 'langchain'
+import { createEthereumCoreTransactionSchema } from '@/prompt/ethereum/core.transaction'
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+} from "langchain/prompts";
+import { FlipsideSchema } from '@/schema/interface';
+import { ChatOpenAI } from "langchain/chat_models";
+
+// TODO: refactor to have the dependency injection
+const chat = new ChatOpenAI({ temperature: 0, maxConcurrency: 5 });
+
+type Data = {
+  name: string
+}
+
+const callGPT = async (schema: FlipsideSchema, rawUserMessage: string) => {
+  const chatPromptTemplate = schema.toChatPromptTemplate();
+  const response = await chat.generatePrompt([
+    await chatPromptTemplate.formatPromptValue({
+      userMessage: rawUserMessage,
+    }),
+  ]);
+  console.log('response', response);
+
+  // TODO: refactor this for much human readable way
+  return response.generations.map((g) => g.map((m) => m.text).join(' '))[0];
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'POST') {
+    const rawUserMessage = req.body.userMessage;
+    const ethCoreTxSchema = createEthereumCoreTransactionSchema();
+    const modelResponse: string = await callGPT(ethCoreTxSchema, rawUserMessage);
+    res.status(200).json({ name: modelResponse })
+  } else {
+    res.status(405).json({ name: 'Method Not Allowed' })
+  }
+}
---
+++ src/schema/interface.ts
@@ -0,0 +1,47 @@
+import { PromptTemplate, PromptTemplateInput } from "langchain/prompts";
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+} from "langchain/prompts";
+
+export class FlipsideSchema {
+  private ChainName: string;
+  private SchemaName: string;
+  private TableName: string;
+  private TableSchema: string;
+
+  constructor(ChainName: string, SchemaName: string, TableName: string, TableSchema: string) {
+    this.ChainName = ChainName;
+    this.SchemaName = SchemaName;
+    this.TableName = TableName;
+    this.TableSchema = TableSchema;
+  }
+
+  // TODO: Remove the strongly combined prompt logic from the schema
+  toChatPromptTemplate(): ChatPromptTemplate {
+    let message =
+      "I want to you to act like the expert who are good at writing SQL query.\n";
+    message += `Given the table below, please write the write the SQL query that can get the info about {userMessage}\n`;
+    message +=
+      "GIVE A SQL QUERY ONLY **without** any further responses/explanations.\n";
+    message +=
+      "If you are not 100% certain to get the valid information from the database table below, respond \"NO\" without further responses/explanations.\n";
+    message += "To query the database, you need to use the database information below\n";
+
+    const tableName = `${this.ChainName}.${this.SchemaName}.${this.TableName}`;
+    message += `\nPlease use the table given below.\n\ntable name: ${tableName}`;
+    message += "Please make sure that transaction hash should be represented as `tx_hash`."
+    message += `\ntable schema: ${this.TableSchema}`;
+    console.log('message', message);
+    const flipsidePrompt = ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(
+        message
+      ),
+      // HumanMessagePromptTemplate.fromTemplate("{text}"),
+    ]);
+    console.log('message2', flipsidePrompt.promptMessages);
+
+    return flipsidePrompt;
+  }
+}

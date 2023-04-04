790356d9c33f236393496a7f45291d8573c03052
mrclo90 <mrclo0116@gmail.com>
2023-04-04 23:12:22 +0900

    check DAU with amplitude

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '@styles/reset.css';
 import '@styles/globals.css';
+import amplitude from 'amplitude-js';
 
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+amplitude.getInstance().init(String(process.env.NEXT_PUBLIC_AMPLITUDE_KEY));
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
